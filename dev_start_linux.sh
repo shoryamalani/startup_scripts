5ec7ad372083555630121a6ae9ebf7ec0f0afe46
Shorya Malani <github-sho@smlist.eml.cc>
2020-09-19 15:29:52 -0400

    adding sitemap

---
+++ dev_start_linux.sh
@@ -0,0 +1,18 @@
+sudo apt upgrade
+sudo apt install python
+sudo apt install fish
+sudo apt install autojump
+echo “if you need java type in this : sudo apt install default-jre”
+sudo apt install xclip
+sudo apt install curl
+curl https://git.io/fisher —create-dirs -sLo ~/.config/fish/functions/fisher.fish
+fisher
+fisher add oh-my-fish/theme-bobthefish
+cd ~/.config/fish
+touch config.fish
+echo “# following file installed after autojump install using brew install autojump” >> ~/.config/fish/config.fish
+echo “source /usr/share/autojump/autojump.fish” >> ~/.config/fish/config.fish
+echo “# activating direnv for virtual environemtns” >> ~/.config/fish/config.fish
+echo “eval (direnv hook fish)” >> ~/.config/fish/config.fish
+set LS_COLORS “di=1;31”
+fish
